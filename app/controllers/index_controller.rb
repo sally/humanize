6755ef6662e2d9f363953a324be0ce286a8bd8a5
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-11-06 12:02:18 -0800

    Add more custom routes (survey, /, tips, thanks)

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,7 +2,18 @@ class IndexController < ApplicationController
   def index
   end
 
+  def dashboard
+
+  end
+
   def survey
 
+  end
+
+  def tips
+
+  end
+
+  def thanks
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'index#index'
 
   scope '/shippo' do
+    get '/', :to => 'index#dashboard'
     get '/survey', :to => 'index#survey'
   end
+
+  get '/tips', :to => 'index#tips'
+  get '/thanks', :to => 'index#thanks'
 end
