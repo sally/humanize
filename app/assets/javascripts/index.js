187db2e078fba864d5e86600edc2e2abb09d7585
spark <spark@MacBook-Air-2.local>
2016-11-06 15:16:07 -0800

    Delay redirect upon submission after showing modal

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+  surveySubmitListener('.survey');
+})
+
+function surveySubmitListener(formClass){
+  $(formClass).on('submit', function(event){
+    event.preventDefault();
+    console.log(":D");
+  })
+}
