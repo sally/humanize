447c168bfd0c825450ccff99382420fdf8da0286
spark <spark@MacBook-Air-2.local>
2016-11-07 12:20:16 -0800

    Add HTML5 sliders and labels of sliders

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -1,13 +1,13 @@
-$(document).on('ready', function(){
-  console.log("We're in")
-
-  $('#q1-slider').slider();
-
-  convertToSlider('#q1-slider');
-  convertToSlider('#q2-slider');
-  convertToSlider('#q3-slider');
-})
-
-function convertToSlider(divId){
-  $(divId).slider();
-}
+// $(document).on('ready', function(){
+//   console.log("We're in")
+//
+//   $('#q1-slider').slider();
+//
+//   convertToSlider('#q1-slider');
+//   convertToSlider('#q2-slider');
+//   convertToSlider('#q3-slider');
+// })
+//
+// function convertToSlider(divId){
+//   $(divId).slider();
+// }
