710a59d7c7157ae23b781a96dc216e4755ee3ec2
spark <spark@MacBook-Air-2.local>
2016-11-07 20:34:55 -0800

    Add swiping left upon click of next button on first survey screen

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -1,13 +1,17 @@
-// $(document).on('ready', function(){
-//   console.log("We're in")
-//
-//   $('#q1-slider').slider();
-//
-//   convertToSlider('#q1-slider');
-//   convertToSlider('#q2-slider');
-//   convertToSlider('#q3-slider');
-// })
-//
-// function convertToSlider(divId){
-//   $(divId).slider();
-// }
+$(document).on('ready', function(){
+  console.log("We're in");
+
+  $('#next-survey-btn').click(function() {
+
+    $('#before-survey-questions').animate({
+        left: '-50%'
+    }, 500, function() {
+        $(this).css('left', '150%');
+        $(this).appendTo('#survey-container');
+    });
+
+    $('#after-survey-questions').animate({
+        left: '50%'
+    }, 500);
+});
+})
