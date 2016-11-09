53b6704d949a9781b95329104d5ff61afd07d164
Young Ju Park <parkyngj@gmail.com>
2016-11-09 11:37:14 -0800

    Add Hbot cropped head to assets

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,3 +1,17 @@
+// we're checking to see if the arrow is up or down, then adding or removing the "lift" class accordingly
+var arrowBounce = function() {
+  var arrow = $(".arrow");
+
+  if (arrow.hasClass("lift")) {
+    arrow.removeClass("lift");
+  } else {
+    arrow.addClass("lift");
+  }
+};
+
+// run the arrowBounce function every 800ms
+setInterval(arrowBounce, 800);
+
 // $(document).on('ready', function(){
 //   var ctrl = new ScrollMagic.Controller({
 //     globalSceneOptions: {
