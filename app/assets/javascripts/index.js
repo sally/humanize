ef63a793666bd10d5e57c020c45b46d7a34b1eef
Young Ju Park <parkyngj@gmail.com>
2016-11-09 11:01:40 -0800

    Moves old index html to index_prev and comments out index js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,45 +1,45 @@
-$(document).on('ready', function(){
-  var ctrl = new ScrollMagic.Controller({
-    globalSceneOptions: {
-      triggerHook: 'onLeave'
-    }
-  });
-
-  $('section').each(function() {
-
-    var name = $(this).attr('id');
-
-    new ScrollMagic.Scene({
-      triggerElement: this
-    })
-    .setPin(this)
-    .addIndicators({
-      colorStart: "rgba(255,255,255,0.5)",
-      colorEnd: "rgba(255,255,255,0.5)",
-      colorTrigger : "rgba(255,255,255,1)",
-      name:name
-    })
-    .addTo(ctrl)
-  });
-
-  // get window height
-  var wh = window.innerHeight;
-
-  new ScrollMagic.Scene({
-    offset: wh*0.75
-  })
-  .setClassToggle("section#two", "is-active")
-  .addTo(ctrl);
-
-  new ScrollMagic.Scene({
-    offset: wh*1.75
-  })
-  .setClassToggle("section#three", "is-active")
-  .addTo(ctrl);
-
-  new ScrollMagic.Scene({
-    offset: wh*2.75
-  })
-  .setClassToggle("section#four", "is-active")
-  .addTo(ctrl);
-})
+// $(document).on('ready', function(){
+//   var ctrl = new ScrollMagic.Controller({
+//     globalSceneOptions: {
+//       triggerHook: 'onLeave'
+//     }
+//   });
+//
+//   $('section').each(function() {
+//
+//     var name = $(this).attr('id');
+//
+//     new ScrollMagic.Scene({
+//       triggerElement: this
+//     })
+//     .setPin(this)
+//     .addIndicators({
+//       colorStart: "rgba(255,255,255,0.5)",
+//       colorEnd: "rgba(255,255,255,0.5)",
+//       colorTrigger : "rgba(255,255,255,1)",
+//       name:name
+//     })
+//     .addTo(ctrl)
+//   });
+//
+//   // get window height
+//   var wh = window.innerHeight;
+//
+//   new ScrollMagic.Scene({
+//     offset: wh*0.75
+//   })
+//   .setClassToggle("section#two", "is-active")
+//   .addTo(ctrl);
+//
+//   new ScrollMagic.Scene({
+//     offset: wh*1.75
+//   })
+//   .setClassToggle("section#three", "is-active")
+//   .addTo(ctrl);
+//
+//   new ScrollMagic.Scene({
+//     offset: wh*2.75
+//   })
+//   .setClassToggle("section#four", "is-active")
+//   .addTo(ctrl);
+// })
