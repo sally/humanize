7070c656238a3164d66789567edcf77402e6870f
spark <spark@macbook-air-2.attlocal.net>
2016-11-09 00:26:59 -0800

    Style submit button and space survey questions out

--- app/assets/javascripts/survey_submissions.js
+++ app/assets/javascripts/survey_submissions.js
@@ -10,14 +10,30 @@ function surveySubmitListener(formClass){
     event.preventDefault();
     console.log(":D");
 
+    $('#submit-survey-btn').click(function() {
+      $('#after-survey-questions').animate({
+          left: '-50%'
+      }, 500);
+
+      // here is where i would animate another container to swoop in, IF I HAD ONE
+    });
+
     var form = $(this);
     var data = form.serialize();
-    var redirectPath = $(formClass + ' ' + 'input[name="redirect-path"]').val();
 
-    // console.log(redirectPath);
+    
 
-    setTimeout(function () {
-       window.location.href = "/" + redirectPath;
-    }, 5000);
+    // make AJAX request to POST to API
+
+    // change this later to be on AJAX success
+
+    // form had <input type="hidden" name="redirect-path" value="tips"> for pre and <input type="hidden" name="redirect-path" value="splash"> for post
+    // var redirectPath = $(formClass + ' ' + 'input[name="redirect-path"]').val();
+    //
+    // // console.log(redirectPath);
+    //
+    // setTimeout(function () {
+    //    window.location.href = "/" + redirectPath;
+    // }, 5000);
   })
 }
