dd87c8f0ccfe8df62540b130a17da6f6319c9ad2
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-11-09 08:38:18 -0800

    Need to change tips page

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -43,3 +43,17 @@ $(document).on('ready', function(){
   .setClassToggle("section#four", "is-active")
   .addTo(ctrl);
 })
+
+// we're checking to see if the arrow is up or down, then adding or removing the "lift" class accordingly
+var arrowBounce = function() {
+  var arrow = $(".arrow");
+
+  if (arrow.hasClass("lift")) {
+    arrow.removeClass("lift");
+  } else {
+    arrow.addClass("lift");
+  }
+};
+
+// run the arrowBounce function every 800ms
+setInterval(arrowBounce, 800);
