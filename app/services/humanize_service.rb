883a7ccb626e73db026aad1b9b21dc98bfe8dd52
Young Ju Park <parkyngj@gmail.com>
2016-11-09 08:56:59 -0800

    Consolidate wrapper method to return responders and filter responders into one method

--- app/services/humanize_service.rb
+++ app/services/humanize_service.rb
@@ -1,9 +1,7 @@
 module HumanizeService
   include HTTParty
 
-  # API_ENDPOINT = "http://humanize-api.herokuapp.com/api/v1"
-
-  API_ENDPOINT = "http://localhost:3000/api/v1"
+  API_ENDPOINT = "http://humanize-api.herokuapp.com/api/v1"
 
   # Method to get all companies
   def self.get_companies
@@ -28,34 +26,44 @@ module HumanizeService
     response = HTTParty.get(url)
   end
 
-  # Method to get responders by session
-  def self.get_session_responders(company_name, session_id)
-    company_id = get_company_id(company_name)
-    url = API_ENDPOINT + "/companies/#{company_id}/sessions/#{session_id}/responders"
-    response = HTTParty.get(url)
-  end
-
-  # Method to get before-responders by sessions
-  def self.get_session_responders_before(company_name, session_id)
-    company_id = get_company_id(company_name)
-    url = API_ENDPOINT + "/companies/#{company_id}/sessions/#{session_id}/responders?before=true"
-    response = HTTParty.get(url)
-  end
-
-  # Method to get after-responders by sessions
-  def self.get_session_responders_after(company_name, session_id)
-    company_id = get_company_id(company_name)
-    url = API_ENDPOINT + "/companies/#{company_id}/sessions/#{session_id}/responders?before=false"
-    response = HTTParty.get(url)
-  end
-
-  # Method to get all gender responses from a session
-  # (get top 4 genders)
-  
-
+  # # Method to get responders by session
+  # def self.get_session_responders(company_name, session_id)
+  #
+  # end
+  #
+  # # Method to get before-responders by sessions
+  # def self.get_session_responders_before(company_name, session_id)
+  #   company_id = get_company_id(company_name)
+  #   url = API_ENDPOINT + "/companies/#{company_id}/sessions/#{session_id}/responders?before=true"
+  #   response = HTTParty.get(url)
+  # end
+  #
+  # # Method to get after-responders by sessions
+  # def self.get_session_responders_after(company_name, session_id)
+  #   company_id = get_company_id(company_name)
+  #   url = API_ENDPOINT + "/companies/#{company_id}/sessions/#{session_id}/responders?before=false"
+  #   response = HTTParty.get(url)
+  # end
 
   # Method to get responses by age group from a session
   # Age groups: 18-25, 25-40, 40-65, 65+
+  def self.get_session_responders(company_name, session_id, args = {})
+    company_id = get_company_id(company_name)
+    url = API_ENDPOINT + "/companies/#{company_id}/sessions/#{session_id}/responders"
+
+    if args.empty?
+      response = HTTParty.get(url)
+    else
+      url += "?"
+
+      args.keys.each_with_index do |attr, i|
+        url += attr.to_s + "=" + args.values[i].to_s + "&"
+        p url
+      end
+
+      response = HTTParty.get(url)
+    end
+  end
 
   # Method to get responders by position level from a session
   # Position levels: C-Level, Senior, Manager, Staff
