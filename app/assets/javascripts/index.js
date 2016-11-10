82e2693898124d1a252ddd9720f1d834f47dd0c5
Young Ju Park <parkyngj@gmail.com>
2016-11-10 10:55:51 -0800

    Animates icons on homepage to zoom in upon scroll to

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -18,6 +18,7 @@ $(window).scroll(function(){
   });
 
 $(document).on('ready', function(){
+  // smooth scroll for nav links
   smoothScroll.init({
     selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
     selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
@@ -25,5 +26,46 @@ $(document).on('ready', function(){
     easing: 'easeInOutCubic', // Easing pattern to use
     offset: 200, // Integer. How far to offset the scrolling anchor location in pixels
     callback: function ( anchor, toggle ) {} // Function to run after scrolling
-});
+  });
+
+  // animate icons on scroll to fade in / fade up
+  $(window).scroll(function(){
+    var hT = $('.info-container-one').offset().top,
+       hH = $('.info-container-one').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+    console.log((hT-wH) , wS);
+    if ((wS) > (hT+hH-wH)){
+     $('.info-icon-one').removeClass('icon-hidden');
+     $('.info-icon-one').addClass('zoomIn');
+     $('.info-icon.one').css('color', '#666')
+    }
+  })
+
+  $(window).scroll(function(){
+    var hT = $('.info-container-two').offset().top,
+       hH = $('.info-container-two').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+    console.log((hT-wH) , wS);
+    if ((wS) > (hT+hH-wH)){
+     $('.info-icon-two').removeClass('icon-hidden');
+     $('.info-icon-two').addClass('zoomIn');
+     $('.info-icon.two').css('color', '#666')
+    }
+
+    $(window).scroll(function(){
+      var hT = $('.info-container-three').offset().top,
+         hH = $('.info-container-three').outerHeight(),
+         wH = $(window).height(),
+         wS = $(this).scrollTop();
+      console.log((hT-wH) , wS);
+      if ((wS) > (hT+hH-wH)){
+       $('.info-icon-three').removeClass('icon-hidden');
+       $('.info-icon-three').addClass('zoomIn');
+       $('.info-icon.three').css('color', '#666')
+      }
+    })
+  })
+
 })
