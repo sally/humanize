1e2c41e110771a6d930ecd0b0cf4c183c52f4adf
Young Ju Park <parkyngj@gmail.com>
2016-11-09 22:33:16 -0800

    AJAXes new title and formatted date upon click of back button

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -6,6 +6,8 @@ $(document).on('ready', function(){
 
     var currentDate = currentUnformattedDate.substring(6,10) + "-" + currentUnformattedDate.substring(0,2) + "-" + currentUnformattedDate.substring(3,5);
 
+    console.log(currentDate);
+
     request1 = $.ajax({
       method: 'GET',
       url: '/sessioninfo?current_date=' + currentDate
@@ -28,9 +30,11 @@ $(document).on('ready', function(){
     response2 = request2.done(function(response){
       var response_array = response.split("&")
       var title = response_array[0]
-      var date = response_array[1]
+      var unformatted_date = response_array[1]
+      var date = unformatted_date.substring(5,7) + "-" + unformatted_date.substring(8,10) + "-" + unformatted_date.substring(0,4)
       $('#ee-title-container').text(title);
       $('#ee-subtitle-container').text(date);
+      $('.arrow-right').css('color', '#000');
     })
   });
 })
