d1b0a1f58c0e49ff15f05d8476fa7ce8781caabc
Young Ju Park <parkyngj@gmail.com>
2016-11-14 10:10:36 -0800

    Enable POST request upon survey submit

--- app/assets/javascripts/survey_submissions.js
+++ app/assets/javascripts/survey_submissions.js
@@ -15,95 +15,95 @@ function surveySubmitListener(formClass){
       // here is where i would animate another container to swoop in, IF I HAD ONE
 
     // *** disable ajax request to make new objects in db for preso ***
-    // var form = $(this);
-    // var data = form.serialize();
-    //
-    // var responderData = form.find('input', 'select').filter('.survey-question').serialize();
-    // var responseOneData = form.find('input').filter('#reflection-question-one').serialize();
-    // var responseTwoData = form.find('input').filter('#reflection-question-two').serialize();
-    // var responseThreeData = form.find('input').filter('#reflection-question-three').serialize();
-    //
-    // var responseOneData = responseOneData + "&%5Bresponse%5Dquestion_id=1"
-    // var responseTwoData = responseTwoData + "&%5Bresponse%5Dquestion_id=2"
-    // var responseThreeData = responseThreeData + "&%5Bresponse%5Dquestion_id=3"
-    //
-    // var request = $.ajax({
-    //   method: 'POST',
-    //   url: 'http://humanize-api.herokuapp.com/api/v1/companies/1/sessions/1/responders',
-    //   data: responderData
-    // });
-    //
-    // var response = request.done(function(response){
-    //   var responderId = response.id;
-    //   var responderBefore = response.before;
-    //
-    //   // Do nested AJAX call to make Response objects out of Responder ID
-    //   var request1 = $.ajax({
-    //     method: 'POST',
-    //     url: 'http://humanize-api.herokuapp.com/api/v1/companies/1/sessions/1/responses',
-    //     data: responseOneData + "&%5Bresponse%5Dresponder_id=" + responderId
-    //   });
-    //
-    //   response1 = request1.done(function(response){ });
-    //
-    //   var request2 = $.ajax({
-    //     method: 'POST',
-    //     url: 'http://humanize-api.herokuapp.com/api/v1/companies/1/sessions/1/responses',
-    //     data: responseTwoData + "&%5Bresponse%5Dresponder_id=" + responderId
-    //   });
-    //
-    //   response2 = request2.done(function(response){ });
-    //
-    //   var request3 = $.ajax({
-    //     method: 'POST',
-    //     url: 'http://humanize-api.herokuapp.com/api/v1/companies/1/sessions/1/responses',
-    //     data: responseThreeData + "&%5Bresponse%5Dresponder_id=" + responderId
-    //   });
-    //
-    //   response3 = request3.done(function(response){ });
-    //
-    //   /* Depending on whether the survey was before or after EE session, inject the appropriate page */
-    //   if(responderBefore){
-    //     $.ajax({
-    //       method: 'GET',
-    //       url: '/tips'
-    //     })
-    //     .done(function(response){
-    //       $('.survey-container').replaceWith(response);
-    //     })}
-    //   else{
-    //     $.ajax({
-    //       method: 'GET',
-    //       url: '/thankyou'
-    //     })
-    //     .done(function(response){
-    //       $('.survey-container').replaceWith(response);
-    //     })
-    //   }
-    // });
+    var form = $(this);
+    var data = form.serialize();
+
+    var responderData = form.find('input', 'select').filter('.survey-question').serialize();
+    var responseOneData = form.find('input').filter('#reflection-question-one').serialize();
+    var responseTwoData = form.find('input').filter('#reflection-question-two').serialize();
+    var responseThreeData = form.find('input').filter('#reflection-question-three').serialize();
+
+    var responseOneData = responseOneData + "&%5Bresponse%5Dquestion_id=1"
+    var responseTwoData = responseTwoData + "&%5Bresponse%5Dquestion_id=2"
+    var responseThreeData = responseThreeData + "&%5Bresponse%5Dquestion_id=3"
+
+    var request = $.ajax({
+      method: 'POST',
+      url: 'http://humanize-api.herokuapp.com/api/v1/companies/1/sessions/1/responders',
+      data: responderData
+    });
+
+    var response = request.done(function(response){
+      var responderId = response.id;
+      var responderBefore = response.before;
+
+      // Do nested AJAX call to make Response objects out of Responder ID
+      var request1 = $.ajax({
+        method: 'POST',
+        url: 'http://humanize-api.herokuapp.com/api/v1/companies/1/sessions/1/responses',
+        data: responseOneData + "&%5Bresponse%5Dresponder_id=" + responderId
+      });
+
+      response1 = request1.done(function(response){ });
+
+      var request2 = $.ajax({
+        method: 'POST',
+        url: 'http://humanize-api.herokuapp.com/api/v1/companies/1/sessions/1/responses',
+        data: responseTwoData + "&%5Bresponse%5Dresponder_id=" + responderId
+      });
+
+      response2 = request2.done(function(response){ });
+
+      var request3 = $.ajax({
+        method: 'POST',
+        url: 'http://humanize-api.herokuapp.com/api/v1/companies/1/sessions/1/responses',
+        data: responseThreeData + "&%5Bresponse%5Dresponder_id=" + responderId
+      });
+
+      response3 = request3.done(function(response){ });
+
+      /* Depending on whether the survey was before or after EE session, inject the appropriate page */
+      if(responderBefore){
+        $.ajax({
+          method: 'GET',
+          url: '/tips'
+        })
+        .done(function(response){
+          $('.survey-container').replaceWith(response);
+        })}
+      else{
+        $.ajax({
+          method: 'GET',
+          url: '/thankyou'
+        })
+        .done(function(response){
+          $('.survey-container').replaceWith(response);
+        })
+      }
+    });
 
     // *** for demo purposes, just load tips or thank you page depending on the value of input[name='[responder]before'] ***
 
-    var responderType = JSON.parse($("input[name='responder[before]']").val());
-
-    if (responderType){
-      $.ajax({
-        method: 'GET',
-        url: '/tips'
-      })
-      .done(function(response){
-        $('.survey-container').replaceWith(response);
-      })
-    }
-    else{
-      $.ajax({
-        method: 'GET',
-        url: '/thankyou'
-      })
-      .done(function(response){
-        $('.survey-container').replaceWith(response);
-      })
-    };
+    // var responderType = JSON.parse($("input[name='responder[before]']").val());
+    //
+    // if (responderType){
+    //   $.ajax({
+    //     method: 'GET',
+    //     url: '/tips'
+    //   })
+    //   .done(function(response){
+    //     $('.survey-container').replaceWith(response);
+    //   })
+    // }
+    // else{
+    //   $.ajax({
+    //     method: 'GET',
+    //     url: '/thankyou'
+    //   })
+    //   .done(function(response){
+    //     $('.survey-container').replaceWith(response);
+    //   })
+    // };
 
 
 
